78ec4cf6ac0750584b65293d1e8a0423b1010ae9
Angga R <angga@linuxmail.org>
2023-11-04 06:06:57 +0700

    ServerPy: make it with emoji
    Because @anulalat like this
    
    Change-Id: I76ed40f8181d3278ffce2ede4e2cff8cbf0afa7d
    Signed-off-by: Angga <angga@linuxmail.org>

--- server.py
+++ server.py
@@ -23,9 +23,7 @@ with open("config.env") as f:
         elif line.startswith("BOT_TOKEN"):
             BOT_TOKEN = line.strip().split("=")[1].strip().replace("'", "")
         elif line.startswith("ALLOWED_USERS"):
-            ALLOWED_USERS = (
-                line.strip().split("=")[1].strip().replace("'", "").split(",")
-            )
+            ALLOWED_USERS = line.strip().split("=")[1].strip().replace("'", "").split(',')
 
 # Handle send_message
 sent_messages = {}
@@ -41,14 +39,12 @@ else:
 
 logger.info("Bot started and running...")
 
-
 def get_system_uptime():
     boot_time_timestamp = psutil.boot_time()
     current_time = datetime.now().timestamp()
     uptime_seconds = current_time - boot_time_timestamp
     uptime_str = str(timedelta(seconds=uptime_seconds))
     return uptime_str
-
 
 def get_public_ip():
     response = requests.get("https://api64.ipify.org?format=json")
@@ -58,7 +54,6 @@ def get_public_ip():
         return public_ip
     else:
         return None
-
 
 def get_ip_details(ip_address):
     # Get IP details using the free IPinfo.io service
@@ -70,7 +65,6 @@ def get_ip_details(ip_address):
         return data
     else:
         return None
-
 
 def get_system_info_output():
     # Get uptime info
@@ -90,7 +84,7 @@ def get_system_info_output():
 
     # Get memory information
     memory = psutil.virtual_memory()
-    total_memory = memory.total // (1024 ** 2)  # Convert to MiB
+    total_memory = memory.total // (1024**2)  # Convert to MiB
 
     # Get OS information
     os_info = platform.platform()
@@ -129,38 +123,37 @@ def get_system_info_output():
     # Function to get the network status as part of system information
     def get_network_info():
         mb_sent, mb_received = get_bandwidth_info()
-        network_info = f"In: {mb_received:.2f} MB / Out: {mb_sent:.2f} MB\n"
+        network_info = f"📥 Incoming: {mb_received:.2f} MB\n📤 Outgoing: {mb_sent:.2f} MB\n"
         return network_info
 
     # Generate the output
     network_info = get_network_info()
-    output = "**System Info**\n"
-    output += "**-----------**\n"
+    output = "ℹ️ **System Info**\n"
     if public_ip:
-        output += f"**IP Address** `{public_ip}`\n"
+        output += f"🌐 **IP Address** `{public_ip}`\n"
         if ip_details:
-            output += f"**City** `{ip_details.get('city')}`\n"
-            output += f"**Region** `{ip_details.get('region')}`\n"
-            output += f"**Country** `{ip_details.get('country')}`\n"
-            output += f"**Location** `{ip_details.get('loc')}`\n"
+            output += f"🏙️ **City** `{ip_details.get('city')}`\n"
+            output += f"🌍 **Region** `{ip_details.get('region')}`\n"
+            output += f"🌐 **Country** `{ip_details.get('country')}`\n"
+            output += f"📍 **Location** `{ip_details.get('loc')}`\n"
         else:
-            output += "Unable to retrieve IP details for the public IP.\n"
-    output += f"**Processor** `{cpu_info}`\n"
-    output += f"**CPU Usage** `{cpu_usage}%`\n"
-    output += f"**CPU Cores** `{cpu_count_str}`\n"
-    output += f"**CPU Freq** `@{cpu_freq:.3f} MHz`\n"
-    output += f"**Disk Usage** `{disk_used:.2f} GB / {disk_total:.2f} GB ({disk_percent:.2f}%)`\n"
-    output += f"**Memory** `{total_memory} MiB`\n"
+            output += "🚫 Unable to retrieve IP details for the public IP.\n"
+    output += f"💻 **Processor** `{cpu_info}`\n"
+    output += f"📊 **CPU Usage** `{cpu_usage}%`\n"
+    output += f"🧠 **CPU Cores** `{cpu_count_str}`\n"
+    output += f"⏰ **CPU Freq** `@{cpu_freq:.3f} MHz`\n"
+    output += f"💽 **Disk Usage** `{disk_used:.2f} GB / {disk_total:.2f} GB ({disk_percent:.2f}%)`\n"
+    output += f"💾 **Memory** `{total_memory} MiB`\n"
     output += (
-        f"**RAM Usage** `{ram_used:.2f} GB / {ram_total:.2f} GB ({ram_percent}%)`\n"
+        f"📈 **RAM Usage** `{ram_used:.2f} GB / {ram_total:.2f} GB ({ram_percent}%)`\n"
     )
-    output += f"**Swap** `{swap_used:.2f} GB / {swap_total:.2f} GB ({swap_percent}%)`\n"
-    output += f"**Network info** `{network_info}`"
-    output += f"**Uptime** `{uptime_str}`\n\n"
-    output += f"**OS** `{os_info}`\n"
-    output += f"**Arch** `{platform.architecture()[0]} (64 Bit)`\n"
-    output += f"**Kernel** `{kernel_version}`\n"
-    output += f"**Hostname** `{hostname}`\n"
+    output += f"🔄 **Swap** `{swap_used:.2f} GB / {swap_total:.2f} GB ({swap_percent}%)`\n"
+    output += f"📶 **Network info:** \n`{network_info}`\n"
+    output += f"⏳ **Uptime** `{uptime_str}`\n\n"
+    output += f"🖥️ **OS** `{os_info}`\n"
+    output += f"💿 **Arch** `{platform.architecture()[0]} (64 Bit)`\n"
+    output += f"🏠 **Kernel** `{kernel_version}`\n"
+    output += f"🏢 **Hostname** `{hostname}`\n"
 
     return output
 
@@ -168,32 +161,30 @@ def get_system_info_output():
 @client.on(events.NewMessage(pattern="/start"))
 async def start_handler(event):
     if str(event.sender_id) in ALLOWED_USERS:
-        info_button = Button.inline("Info", b"info")
-        help_button = Button.inline("Help", b"help")
+        info_button = Button.inline("🏠 Info", b"info")
+        help_button = Button.inline("❓ Help", b"help")
         start_text = f"Welcome to the server status bot! Use the /status command to get server information."
         x = await event.respond(start_text, buttons=[[info_button], [help_button]])
         await asyncio.sleep(30)
         await x.delete()
     else:
         user_id = event.sender_id
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 @client.on(events.NewMessage(pattern="/status"))
 async def status_handler(event):
     if str(event.sender_id) in ALLOWED_USERS:
         uptime = get_system_uptime()
         system_info_output = get_system_info_output()
-        refresh_button = Button.inline("Refresh", b"refresh")
-        info_button = Button.inline("Info", b"info")
-        help_button = Button.inline("Help", b"help")
+        refresh_button = Button.inline("🔄 Refresh", b"refresh")
+        info_button = Button.inline("🏠 Info", b"info")
+        help_button = Button.inline("❓ Help", b"help")
         buttons = [[refresh_button, info_button, help_button]]
         sent_message = await event.respond(system_info_output, buttons=buttons)
         sent_messages[event.chat_id] = sent_message
     else:
         user_id = event.sender_id
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 @client.on(events.InlineQuery)
 async def inline_query_handler(event):
@@ -202,23 +193,22 @@ async def inline_query_handler(event):
         if query == "status":
             uptime = get_system_uptime()
             system_info_output = get_system_info_output()
-            refresh_button = Button.inline("Refresh", b"refresh")
-            info_button = Button.inline("Info", b"info")
-            help_button = Button.inline("Help", b"help")
+            refresh_button = Button.inline("🔄 Refresh", b"refresh")
+            info_button = Button.inline("🏠 Info", b"info")
+            help_button = Button.inline("❓ Help", b"help")
             buttons = [[refresh_button, info_button, help_button]]
             result = await event.builder.article(
                 title="Status Info",
                 description="Show server status info.",
                 text=system_info_output,
-                buttons=buttons,
+                buttons=buttons
             )
             await event.answer([result])
         else:
             await event.answer([])  # No results for unrecognized queries
     else:
         user_id = event.sender_id
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 @client.on(events.CallbackQuery())
 async def callback_handler(event):
@@ -231,7 +221,6 @@ async def callback_handler(event):
     elif callback_data == "help":
         await help_handler(event)
 
-
 async def help_handler(event):
     if str(event.sender_id) in ALLOWED_USERS:
         help_text = (
@@ -239,30 +228,29 @@ async def help_handler(event):
         )
         help_text += "/start - Start the bot and see a welcome message\n"
         help_text += "/status - Get the current server status"
-        refresh_button = Button.inline("Home", b"refresh")
-        info_button = Button.inline("Info", b"info")
+        refresh_button = Button.inline("🏠 Home", b"refresh")
+        info_button = Button.inline("❓ Info", b"info")
 
         [[refresh_button, info_button]]
-        await event.edit(help_text, buttons=[[refresh_button, info_button]])
+        await event.edit(
+            help_text, buttons=[[refresh_button, info_button]]
+        )
     else:
         user_id = event.sender_id
         sent_messages = {}
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 async def refresh_handler(event):
     if str(event.sender_id) in ALLOWED_USERS:
         system_info_output = get_system_info_output()
-        refresh_button = Button.inline("Refresh", b"refresh")
-        info_button = Button.inline("Info", b"info")
-        help_button = Button.inline("Help", b"help")
+        refresh_button = Button.inline("🔄 Refresh", b"refresh")
+        info_button = Button.inline("🏢 Info", b"info")
+        help_button = Button.inline("❓ Help", b"help")
 
         buttons = [[refresh_button, info_button, help_button]]
 
         current_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        server_info_with_timestamp = (
-            f"{system_info_output}\n\nLast Refreshed: {current_timestamp}"
-        )
+        server_info_with_timestamp = f"{system_info_output}\n\n🕒 Last Refreshed: {current_timestamp}"
 
         await event.edit(
             server_info_with_timestamp,
@@ -271,23 +259,22 @@ async def refresh_handler(event):
     else:
         user_id = event.sender_id
         sent_messages = {}
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 async def info_handler(event):
     if str(event.sender_id) in ALLOWED_USERS:
         uname = platform.uname()
-        softw = "**System Information**\n"
-        softw += f"`System   : {uname.system}`\n"
-        softw += f"`Release  : {uname.release}`\n"
-        softw += f"`Version  : {uname.version}`\n"
-        softw += f"`Machine  : {uname.machine}`\n"
+        softw = "🖥️ **System Information**\n"
+        softw += f"🏢 **System**   : `{uname.system}`\n"
+        softw += f"📅 **Release**  : `{uname.release}`\n"
+        softw += f"🔄 **Version**  : `{uname.version}`\n"
+        softw += f"💻 **Machine**  : `{uname.machine}`\n"
 
         uptime = get_system_uptime()
-        softw += f"`Uptime   : {uptime}`\n"
+        softw += f"⏳ **Uptime**   : `{uptime}`\n"
 
-        refresh_button = Button.inline("Home", b"refresh")
-        help_button = Button.inline("Help", b"help")
+        refresh_button = Button.inline("🏠 Home", b"refresh")
+        help_button = Button.inline("❓ Help", b"help")
 
         buttons = [[refresh_button, help_button]]
 
@@ -297,8 +284,7 @@ async def info_handler(event):
     else:
         user_id = event.sender_id
         sent_messages = {}
-        await client.send_message(user_id, "You are not allowed to use this bot.")
-
+        await client.send_message(user_id, "🚫 You are not allowed to use this bot.")
 
 # Run the client until disconnected
 client.run_until_disconnected()
